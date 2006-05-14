20f5baeca0d38267d23b9b87a8b3623321690ae5
Xavier Maillard <xma@gnu.org>
2006-05-14 21:35:18 +0000

    Copyright update
    * Added header file
    * Added legal notice

--- color-theme-library.el
+++ color-theme-library.el
@@ -1,5 +1,32 @@
+;;; color-theme-library.el --- The real color theme functions
 
-;;; The color theme functions
+;; Copyright (C) 2005, 2006  Xavier Maillard <zedek@gnu.org>
+;; Copyright (C) 2005, 2006  Brian Palmer <bpalmer@gmail.com>
+
+;; Version: 0.0.9
+;; Keywords: faces
+;; Author: Brian Palmer, Xavier Maillard
+;; Maintainer: Xavier Maillard <zedek@gnu.org>
+;; URL: http://www.emacswiki.org/cgi-bin/wiki.pl?ColorTheme
+
+;; This file is not (YET) part of GNU Emacs.
+
+;; This is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 2, or (at your option) any later
+;; version.
+;;
+;; This is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+;; for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+;; MA 02111-1307, USA.
+
+;; Code:
 
 (defun color-theme-gnome ()
   "Wheat on darkslategrey scheme.
@@ -13506,3 +13533,5 @@ Created by Dave Benjamin <ramen@ramenfest.com> Dec 23 2005."
     (trailing-whitespace ((t (:background "red"))))
     (underline ((t (:underline t))))
     (variable-pitch ((t (:family "helv")))))))
+
+;;; color-theme-library.el ends here
